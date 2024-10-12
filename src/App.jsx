0ed7b1d0b8a0be5080f1f81c06dd8f231e912ba3
Faisal <faisalahmed4315@gmail.com>
2024-10-12 14:17:35 +0600

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Application</h1>
+
+     <Home></Home>
+    </>
+  )
+}
+
+export default App
